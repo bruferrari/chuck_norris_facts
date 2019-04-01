84fccae32850d154eac2409ea1e7ec8abf926835
Bruno Ferrari <b.ferrari09@gmail.com>
2019-04-01 14:49:30 -0300

    fix usecase

--- app/src/main/java/com/bferrari/stonechallenge/StoneChallengeApplication.kt
+++ app/src/main/java/com/bferrari/stonechallenge/StoneChallengeApplication.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.bferrari.data.injection.apiModule
 import com.bferrari.data.injection.dataModule
 import com.bferrari.stonechallenge.injection.appModule
+import com.bferrari.usecases.injection.useCaseModules
 import org.koin.android.ext.android.startKoin
 
 
@@ -12,6 +13,6 @@ class StoneChallengeApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        startKoin(this, listOf(apiModule, appModule, dataModule))
+        startKoin(this, listOf(useCaseModules, apiModule, appModule, dataModule))
     }
 }
